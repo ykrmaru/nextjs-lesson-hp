bd177a974f58c2b38e9844b85036a7340149e5e4
丸山由香里 <ymaruyama@interlink.ne.jp>
2021-04-28 16:33:27 +0900

    オブジェクトでなくていいところを修正

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -48,7 +48,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { post: post } = await getPostData(params.id);
+  const post = await getPostData(params.id);
 
   return {
     props: {
