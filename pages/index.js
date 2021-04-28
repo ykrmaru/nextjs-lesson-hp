174c155cde2ae60ac497c80e9147c8a5e3a9a7e7
丸山由香里 <ymaruyama@interlink.ne.jp>
2021-04-28 16:09:50 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout title='Home'>
+      <p className='text-4xl'>Welcome to Next.js</p>
+    </Layout>
+  );
+}
